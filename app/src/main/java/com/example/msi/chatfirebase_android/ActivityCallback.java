932b6718472714d2c154c86a955448a0c38c673c
bach <bachsmile1997@gmail.com>
2018-12-18 22:09:30 +0700

    pt

---
+++ app/src/main/java/com/example/msi/chatfirebase_android/ActivityCallback.java
@@ -0,0 +1,11 @@
+package com.example.msi.chatfirebase_android;
+
+/**
+ * Class responsible to register all the callbacks necessary
+ * for the application
+ */
+public interface ActivityCallback {
+    void openChat();
+    void openCreateAccount();
+    void logout();
+}
